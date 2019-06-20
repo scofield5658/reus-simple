a81e3fac409e4aa6329e7413f4955010da80cf6b
guozhi <zhi.guo@geekthings.com.cn>
2019-06-20 17:27:30 +0800

    [update] proxy example

--- src/routers/hello.js
+++ src/routers/hello.js
@@ -5,4 +5,10 @@ module.exports = [
     controller: require('../controllers/hello'),
     middlewares: [require('../middlewares/hello.mid')],
   },
+  {
+    path: '/redirect_sample',
+    method: 'get',
+    redirect: '/v1/business/banners',
+    target: 'http://172.50.1.182:3080',
+  },
 ];
