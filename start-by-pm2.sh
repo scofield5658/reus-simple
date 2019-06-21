440ef9a9b41126448d8878bb4b90268aec227c2d
guozhi <zhi.guo@geekthings.com.cn>
2019-06-21 14:29:04 +0800

    [update] finish simple-example

--- start-by-pm2.sh
+++ start-by-pm2.sh
@@ -4,4 +4,4 @@ cd $CUR_DIR
 
 pm2 delete reus-simple-starter
 
-NODE_ENV=production REUS_PROJECT_DIR=${CUR_DIR} pm2 start ./node_modules/reus.js/bin/app.js -i 2 -n reus-simple-starter
+REUS_PROJECT_ENV=prod REUS_PROJECT_DIR=${CUR_DIR} pm2 start ./node_modules/reus.js/bin/app.js -i 2 -n reus-simple-starter
